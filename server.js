2c8b1057f5543a6c7e7e2991be8abbb100a3fbe1
Caroline Sampaio <caroline_08022@hotmail.com>
2024-10-23 17:05:06 -0300

    feat: adiciona configs swagger server.js

--- server.js
+++ server.js
@@ -1,8 +1,35 @@
 import express from "express";
+import swaggerjsdoc from "swagger-jsdoc"
+import swaggerUi from "swagger-ui-express"
+
 import db from "./app/models/index.js";
 import { routes } from "./app/routes/index.js"
 
+// config base swagger
+const swaggerOptions = {
+  swaggerDefinition: {
+      openapi: '3.0.0',
+      info: {
+          title: 'EduDataHub API',
+          description: 'API para gestão de dados escolares.',
+          contact: {
+               name: 'EduDataHub Team'
+          },
+      },
+      servers: [
+          {
+              url: "http://localhost:8080"
+          }
+      ],
+  },
+  apis: ['./swaggerDocs.yml']
+}
+
 const app = express();
+
+// config documentacao
+const swaggerDocs = swaggerjsdoc(swaggerOptions)
+app.use('/api/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs))
 
 // interpreta requisições do tipo json
 app.use(express.json());
